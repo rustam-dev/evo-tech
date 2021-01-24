ebff06d66ea3b93187ecd3ddbc39fc5eff012f2f
lilyjames-23 <59247627+lilyjames-23@users.noreply.github.com>
2021-01-24 19:23:00 +0500

    copied for local disk

--- build/js/loader.js
+++
@@ -1,7 +0,0 @@
-// preloader
-type="text/javascript">$(window).on('load', function () {
-    var $preloader = $('#p_prldr'),
-        $svg_anm   = $preloader.find('.svg_anm');
-    $svg_anm.fadeOut();
-    $preloader.delay(500).fadeOut('slow');
-});
